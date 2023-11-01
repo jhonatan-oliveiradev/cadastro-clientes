0e8ea6434475dc29541f6aef529cf48b3e98437c
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2023-11-01 08:45:31 -0300

    :construction: feat: Customer Register created

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import {
 	FastifyRequest,
 	FastifyReply
 } from "fastify";
+import { CreateCustomerController } from "./controllers/CreateCustomerController";
 
 export async function routes(
 	fastify: FastifyInstance,
@@ -12,4 +13,11 @@ export async function routes(
 	fastify.get("/test", async (request: FastifyRequest, reply: FastifyReply) => {
 		return { ok: true };
 	});
+
+	fastify.post(
+		"/customer",
+		async (request: FastifyRequest, reply: FastifyReply) => {
+			return new CreateCustomerController().handle(request, reply);
+		}
+	);
 }
